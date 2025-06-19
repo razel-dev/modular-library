cd158b179a34fd708667558ef8314b845ad6a8e4
Alcan <alcan@exemple.com>
2025-06-19 13:36:58 +0100

    methode snakeCase
    # Conflicts:
    #	src/com/modularlib/utils/impl/stringUtils.java

--- src/com/modularlib/utils/impl/stringUtils.java
+++ src/com/modularlib/utils/impl/stringUtils.java
@@ -4,24 +4,40 @@ import com.modularlib.utils.api.IStringutils;
 
 public class stringUtils implements IStringutils {
 	
+<<<<<<< HEAD
 	@override
+=======
+
+>>>>>>> 6432226 (methode snakeCase)
 	public boolean isNullorEmpty (String str) {
 		return str == null ||  str.trim().isEmpty();
 			}
 	
+<<<<<<< HEAD
 	@override 
+=======
+
+>>>>>>> 6432226 (methode snakeCase)
 	public String capitalyze (String str) {
 		if (isNullorEmpty(str)) return str;
 		str = str.trim();
 		return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
 			}
 	
+<<<<<<< HEAD
 	@override
+=======
+	
+>>>>>>> 6432226 (methode snakeCase)
 	public String reverse (String str) {
 	return new StringBuilder(str).reverse().toString();
 	}
 
+<<<<<<< HEAD
 	@override
+=======
+	
+>>>>>>> 6432226 (methode snakeCase)
 	public int countWords (String str) {
 		if (str == null || str.trim().isEmpty()) return 0;
 
@@ -39,6 +55,17 @@ public class stringUtils implements IStringutils {
 	    }
 
 	    return count;	
+	}
+
+
+	public String toSnakeCase(String str) {
+		if (isNullorEmpty(str)) return str;
+		
+		return str
+				.trim()
+				.toLowerCase()
+				.replaceAll("[^a-z0-9\\s]", "")
+			    .replaceAll("\\s+", "_");
 	}
 
 }
